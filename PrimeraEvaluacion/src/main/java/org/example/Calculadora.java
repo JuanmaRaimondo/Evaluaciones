66a6bcea6dd0d7cf6f9de6fd0ecbb3b7facc941b
´´juanmaRaimondo´´ <juanmarraimondo@gmail.com>
2023-12-19 16:32:35 -0300

    Se agrega avances en el ejercicio 1. Faltaria hacer una parte de las excepciones.

--- PrimeraEvaluacion/src/main/java/org/example/Calculadora.java
+++ PrimeraEvaluacion/src/main/java/org/example/Calculadora.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class Calculadora {
+    public double calcular(double numerouno, double numerodos, char operacion){
+        double resultado = 0.0;
+        switch (operacion){
+            case '+':
+                resultado = numerouno + numerodos;
+                break;
+            case '-':
+                resultado = numerouno - numerodos;
+                break;
+            case '*':
+                resultado = numerouno * numerodos;
+                break;
+            case '/':
+                resultado = numerouno / numerodos;
+                break;
+        }
+        return resultado;
+    }
 }
--- PrimeraEvaluacion/src/main/java/org/example/Main.java
+++ PrimeraEvaluacion/src/main/java/org/example/Main.java
@@ -1,4 +1,36 @@
 package org.example;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main (String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Calculadora sencilla");
+        double numero1;
+        double numero2;
+        char operacion;
+
+        try {
+
+
+            System.out.println("Ingrese el primer numero");
+            numero1 = input.nextDouble();
+
+            System.out.println("Ingrese el segundo numero");
+            numero2 = input.nextDouble();
+
+            System.out.println("Ingrese la operacion '+' '-' '/' '*'");
+            operacion = input.next().charAt(0);
+        }catch ( InputMismatchException e ){
+            throw new RuntimeException("Dato ingresado no valido");
+        }
+        Calculadora calcular = new Calculadora();
+        double resultado = calcular.calcular(numero1, numero2, operacion);
+
+        System.out.println("El resultado es " + resultado);
+
 }
+}
+
